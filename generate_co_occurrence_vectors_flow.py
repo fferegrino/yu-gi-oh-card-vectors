9ed3ccb1f339cc3fb2ffe4d4506191e610d67ab7
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-12 16:52:14 +0000

    Generate co-occurrence vectors and Annoy index

--- generate_co_occurrence_vectors_flow.py
+++ generate_co_occurrence_vectors_flow.py
@@ -1,4 +1,4 @@
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, step, Parameter, profile
 
 
 MAIN_DECK_ROW = 6
@@ -19,14 +19,28 @@ def fill_matrix_from_file(csv_file, card_to_id, coo):
                 cards_in_deck.extend(cards_)
 
             for card in cards_in_deck:
+                try:
+                    origin_card = card_to_id[card]
+                except KeyError:
+                    print(f"Origin {card} not found in card_to_id")
+                    continue
+
                 for other_card in cards_in_deck:
                     try:
-                        coo[card_to_id[card], card_to_id[other_card]] += 1
-                    except:
-                        print(f"{card} {other_card}")
+                        destination_card = card_to_id[other_card]
+                    except KeyError:
+                        print(f"Destination {other_card} not found in card_to_id")
+                        continue
+
+                    coo[origin_card, destination_card] += 1
 
 
-class CoOcurrenceMatrixFlow(FlowSpec):
+class GenerateCoOccurrenceVectorsFlow(FlowSpec):
+
+    embedding_size = Parameter("embedding_size", default=20, help="Size of the embedding vectors")
+    annoy_trees = Parameter("annoy_trees", default=5, help="Number of trees to use in the Annoy index")
+    preserve_annoy_index = Parameter("preserve_annoy_index", default=False, help="Preserve the Annoy index")
+
     @step
     def start(self):
         self.cards_repo = "https://github.com/fferegrino/yu-gi-oh.git"
@@ -82,8 +96,41 @@ class CoOcurrenceMatrixFlow(FlowSpec):
             for csv_file in glob.glob(tmpdirname + "/data/*.csv"):
                 fill_matrix_from_file(csv_file, self.card_to_id, coo)
 
-        self.cooccurence_matrix = coo
         self.matrix = coo_matrix(coo)
+
+        self.next(self.build_embeddings)
+
+    @step
+    def build_embeddings(self):
+        from scipy.sparse.linalg import svds
+        import numpy as np
+
+        u, s, v_t = svds(self.matrix.astype(float), k=self.embedding_size)
+        embeddings = u * np.sqrt(s)
+
+        # Normalize embeddings
+        self.embeddings = (embeddings - np.mean(embeddings, axis=0)) / np.std(embeddings, axis=0)
+
+        self.next(self.build_index)
+
+    @step
+    def build_index(self):
+        from annoy import AnnoyIndex
+        from tempfile import NamedTemporaryFile
+
+        with NamedTemporaryFile(delete=not self.preserve_annoy_index) as tmp:
+            ann = AnnoyIndex(self.embedding_size, "angular")
+            ann.on_disk_build(tmp.name)
+            with profile("Add vectors"):
+                for idx, card_vector in enumerate(self.embeddings):
+                    ann.add_item(self.id_to_card[idx], card_vector)
+            with profile("Build index"):
+                ann.build(self.annoy_trees)
+
+            self.model_ann = tmp.read()
+
+            if self.preserve_annoy_index:
+                print(f"Writing index to {tmp.name} and not deleting")
 
         self.next(self.end)
 
@@ -93,4 +140,4 @@ class CoOcurrenceMatrixFlow(FlowSpec):
 
 
 if __name__ == "__main__":
-    CoOcurrenceMatrixFlow()
+    GenerateCoOccurrenceVectorsFlow()
