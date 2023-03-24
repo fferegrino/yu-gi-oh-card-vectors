d47eb2c7778e6a65202fc06136b22c8af9e75947
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-24 10:46:42 +0000

    Optimise co-occurrence

--- yugioh_card_vectors/cooccurrence.py
+++ yugioh_card_vectors/cooccurrence.py
@@ -2,28 +2,28 @@ from itertools import permutations
 from collections import Counter
 import csv
 
-MAIN_DECK_ROW = 6
-EXTRA_DECK_ROW = 7
-SIDE_DECK_ROW = 8
+MAIN_DECK_COLUMN = 6
+EXTRA_DECK_COLUMN = 7
+SIDE_DECK_COLUMN = 8
 
 
-def fill_matrix_from_file(csv_file, card_to_id, coo):
+def fill_matrix_from_file(csv_file, passcode_to_id, coo):
     with open(csv_file) as r:
         reader = csv.reader(r)
         next(reader)
         for row in reader:
             cards_in_deck = []
-            for col in [MAIN_DECK_ROW, EXTRA_DECK_ROW, SIDE_DECK_ROW]:
-                cards = row[col][1:-1].split(",")
-                cards = [int(card_str.strip('"')) for card_str in cards if card_str.strip('"')]
-                cards = [card for card in cards if card in card_to_id]
+            for col in [MAIN_DECK_COLUMN, EXTRA_DECK_COLUMN, SIDE_DECK_COLUMN]:
+                card_ids = row[col][1:-1].split(",")
+                card_ids = [int(card_str.strip('"')) for card_str in card_ids if card_str.strip('"')]
+                card_ids = [passcode_to_id[passcode] for passcode in card_ids if passcode in passcode_to_id]
 
-                cards_in_deck.extend(cards)
+                cards_in_deck.extend(card_ids)
 
-            fill_matrix_with_all_combinations(coo, cards_in_deck, card_to_id)
+            fill_matrix_with_all_combinations(coo, cards_in_deck)
 
 
-def fill_matrix_with_all_combinations(coo_matrix, deck_passcodes, passcode_to_id):
-    all_combinations = Counter(permutations(deck_passcodes, 2))
-    for (card1, card2), count in all_combinations.items():
-        coo_matrix[passcode_to_id[card1], passcode_to_id[card2]] += count
+def fill_matrix_with_all_combinations(coo_matrix, cards_in_deck):
+    all_combinations = Counter(permutations(cards_in_deck, 2))
+    for (card1_id, card2_id), count in all_combinations.items():
+        coo_matrix[card1_id, card2_id] += count
